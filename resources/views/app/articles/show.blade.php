c6517296c22ccc68546141eb71a07b97023a17dd
eliojavier <eliojavier86@gmail.com>
2018-02-22 13:51:58 -0400

    articulos relacionados

--- resources/views/app/articles/show.blade.php
+++ resources/views/app/articles/show.blade.php
@@ -12,151 +12,62 @@
 @section('keywords', $article->keywords)
 
 @section('content')
-    <!-- Start blog section -->
     <section id="blog">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="blog-area">
                         <div class="row">
-                            <div class="col-lg-8 col-md-7 col-sm-12">
+                            <div class="col-lg-9 col-md-9 col-sm-12">
                                 <div class="blog-left blog-details">
                                     <!-- Start single blog post -->
                                     <article class="single-from-blog">
                                         <div class="blog-title">
-                                            <i class="fa fa-user"></i>
                                             <h1>{{$article->title}}</h1>
                                             <p>Publicado por <a href="#" class="blog-admin">{{$article->user->name}}</a>
                                                 en <span class="blog-date">{{$article->created_at}}</span></p>
                                         </div>
                                         <figure>
                                             <a><img style="height: 500px; object-fit: contain"
-                                                                            alt="{{$article->img_name}}" src="{{$article->img_url}}"></a>
+                                                    alt="{{$article->img_name}}" src="{{$article->img_url}}"></a>
                                         </figure>
-                                        {{--<div class="blog-details-content" innerHTML="{{$article->body}}">--}}
                                         <div class="blog-details-content">
                                             {!!html_entity_decode($article->body)!!}
                                         </div>
                                         <p>Categoría: {{$article->categories[0]->name}}</p>
                                     </article>
 
-                                Relacionados:
+                                        <h2 style="margin-left: 25px">Artículos Relacionados:
+                                        </h2>
                                     <br>
-                                @foreach($related_articles->articles as $related_article)
-                                    <b>{{$related_article->title}}</b>
-                                    {{$related_article->img_url}}
-                                    {{$related_article->id}}
-                                        <br>
-                                @endforeach
-                                    <!--<div class="blog-comment">-->
-                                    <!--<h2>Post a comment</h2>-->
-                                    <!--<form class="comment-form" action="">-->
-                                    <!--<div class="form-group">-->
-                                    <!--<input type="text" placeholder="Name" class="form-control">-->
-                                    <!--</div>-->
-                                    <!--<div class="form-group">-->
-                                    <!--<input type="email" placeholder="Enter Email" class="form-control">-->
-                                    <!--</div>-->
-                                    <!--<div class="form-group">-->
-                                    <!--<textarea class="form-control"></textarea>-->
-                                    <!--</div>-->
-                                    <!--<button class="button button-default" data-text="Comment" type="submit"><span>Comment</span></button>-->
-                                    <!--</form>-->
-                                    <!--</div>-->
-                                    <!-- End single blog post -->
+                                    @foreach($related_articles->articles as $related_article)
+                                        <div class="col-md-4">
+                                            <article class="single-from-blog">
+                                                <figure>
+                                                    <a href="{{url('articles/' . $related_article->permalink)}}"><img
+                                                                src="{{$related_article->img_url}}"
+                                                                alt="{{$related_article->img_name}}"></a>
+                                                </figure>
+                                                <div class="blog-title">
+                                                    <h2>
+                                                        <a href="{{url('articles/' . $related_article->permalink)}}">{{$related_article->title}}</a>
+                                                    </h2>
+                                                </div>
+                                                <p>{!!substr(html_entity_decode($related_article->body), 0, 150)!!}
+                                                    ...</p>
+                                            </article>
+                                            <div style="text-align: center!important;">
+                                                <a href="{{url('articles/' . $related_article->permalink)}}"
+                                                   class="button button-default read-more"
+                                                   data-text="Leer más"><span>Leer más</span></a>
+                                            </div>
+                                        </div>
+                                    @endforeach
                                 </div>
                             </div>
-                            <!--<div class="col-lg-4 col-md-5 col-sm-12">-->
-                            <!--<aside class="blog-right">-->
-                            <!--&lt;!&ndash; Start Sidebar Single widget &ndash;&gt;-->
-                            <!--<div class="single-widget">-->
-                            <!--<h2>Search</h2>-->
-                            <!--<form class="blog-search">-->
-                            <!--<input type="text">-->
-                            <!--<button class="button button-default" data-text="Search" type="submit"><span>Search</span></button>-->
-                            <!--</form>-->
-                            <!--</div>-->
-                            <!--&lt;!&ndash; End Sidebar Single widget &ndash;&gt;-->
-                            <!--&lt;!&ndash; Start Sidebar Single widget &ndash;&gt;-->
-                            <!--<div class="single-widget">-->
-                            <!--<h2>Follow us on</h2>-->
-                            <!--<div class="follow-us">-->
-                            <!--<a class="facebook" href="#"><span class="fa fa-facebook"></span></a>-->
-                            <!--<a class="twitter" href="#"><span class="fa fa-twitter"></span></a>-->
-                            <!--<a class="google-plus" href="#"><span class="fa fa-google-plus"></span></a>-->
-                            <!--<a class="youtube" href="#"><span class="fa fa-youtube"></span></a>-->
-                            <!--<a class="linkedin" href="#"><span class="fa fa-linkedin"></span></a>-->
-                            <!--<a class="dribbble" href="#"><span class="fa fa-dribbble"></span></a>-->
-                            <!--</div>-->
-                            <!--</div>-->
-                            <!--&lt;!&ndash; End Sidebar Single widget &ndash;&gt;-->
-                            <!--&lt;!&ndash; Start Sidebar Single widget &ndash;&gt;-->
-                            <!--<div class="single-widget">-->
-                            <!--<h2>Popular post</h2>-->
-                            <!--<div class="popular-post-widget">-->
-                            <!--<div class="media">-->
-                            <!--<div class="media-left">-->
-                            <!--<a href="blog-single.html">-->
-                            <!--<img class="media-object" src="../../assets/images/popular-post-img.jpg" alt="img">-->
-                            <!--</a>-->
-                            <!--</div>-->
-                            <!--<div class="media-body">-->
-                            <!--<h4 class="media-heading"><a href="blog-single.html">A lonely flower</a></h4>-->
-                            <!--<p>Sed ut perspiciatis omnis natus error sit voluptatem accusantium done.</p>-->
-                            <!--</div>-->
-                            <!--</div>-->
-                            <!--<div class="media">-->
-                            <!--<div class="media-left">-->
-                            <!--<a href="blog-single.html">-->
-                            <!--<img class="media-object" src="../../assets/images/popular-post-img.jpg" alt="img">-->
-                            <!--</a>-->
-                            <!--</div>-->
-                            <!--<div class="media-body">-->
-                            <!--<h4 class="media-heading"><a href="blog-single.html">A lonely flower</a></h4>-->
-                            <!--<p>Sed ut perspiciatis omnis natus error sit voluptatem accusantium done.</p>-->
-                            <!--</div>-->
-                            <!--</div>-->
-                            <!--<div class="media">-->
-                            <!--<div class="media-left">-->
-                            <!--<a href="blog-single.html">-->
-                            <!--<img class="media-object" src="../../assets/images/popular-post-img.jpg" alt="img">-->
-                            <!--</a>-->
-                            <!--</div>-->
-                            <!--<div class="media-body">-->
-                            <!--<h4 class="media-heading"><a href="blog-single.html">A lonely flower</a></h4>-->
-                            <!--<p>Sed ut perspiciatis omnis natus error sit voluptatem accusantium done.</p>-->
-                            <!--</div>-->
-                            <!--</div>-->
-                            <!--</div>-->
-                            <!--</div>-->
-                            <!--&lt;!&ndash; End Sidebar Single widget &ndash;&gt;-->
-                            <!--&lt;!&ndash; Start Sidebar Single widget &ndash;&gt;-->
-                            <!--&lt;!&ndash;<div class="single-widget">&ndash;&gt;-->
-                            <!--&lt;!&ndash;<h2>Instagram feed</h2>&ndash;&gt;-->
-                            <!--&lt;!&ndash;<div class="instagram-feed">&ndash;&gt;-->
-                            <!--&lt;!&ndash;<div class="single-instagram-feed">&ndash;&gt;-->
-                            <!--&lt;!&ndash;<img src="assets/images/instagram-feed-img.jpg" alt="img">&ndash;&gt;-->
-                            <!--&lt;!&ndash;</div>&ndash;&gt;-->
-                            <!--&lt;!&ndash;<div class="single-instagram-feed">&ndash;&gt;-->
-                            <!--&lt;!&ndash;<img src="assets/images/instagram-feed-img.jpg" alt="img">&ndash;&gt;-->
-                            <!--&lt;!&ndash;</div>&ndash;&gt;-->
-                            <!--&lt;!&ndash;<div class="single-instagram-feed">&ndash;&gt;-->
-                            <!--&lt;!&ndash;<img src="assets/images/instagram-feed-img.jpg" alt="img">&ndash;&gt;-->
-                            <!--&lt;!&ndash;</div>&ndash;&gt;-->
-                            <!--&lt;!&ndash;</div>&ndash;&gt;-->
-                            <!--&lt;!&ndash;</div>&ndash;&gt;-->
-                            <!--&lt;!&ndash; End Sidebar Single widget &ndash;&gt;-->
-                            <!--&lt;!&ndash; Start Sidebar Single widget &ndash;&gt;-->
-                            <!--<div class="single-widget">-->
-                            <!--<h2>Subscribe to newslater</h2>-->
-                            <!--<form class="blog-search">-->
-                            <!--<input type="text">-->
-                            <!--<button class="button button-default" data-text="Subscribe" type="submit"><span>Subscribe</span></button>-->
-                            <!--</form>-->
-                            <!--</div>-->
-                            <!--&lt;!&ndash; End Sidebar Single widget &ndash;&gt;-->
-                            <!--</aside>-->
-                            <!--</div>-->
+                            <div class="col-md-3">
+
+                            </div>
                         </div>
                     </div>
                 </div>
